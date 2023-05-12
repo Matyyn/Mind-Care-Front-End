37d43bf07aec04893ad9838b6c962261e9d1d0ba
Abdul Mateen <mateen.2k19@gmail.com>
2023-05-12 14:18:49 +0500

    code updated

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -206,7 +206,7 @@ export default function Signup() {
             height="75px"
             width="75px"
             marginTop={"0%"}
-            marginLeft="48%"
+            marginLeft="45%"
           />
           <Text
             fontSize="32"
