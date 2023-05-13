92da2c4b634a79b7442916ca4db29c57899af8bb
Abdul Mateen <mateen.2k19@gmail.com>
2023-05-13 16:13:19 +0500

    Edit Profile Navbar

--- src/components/EditProfile/Navbar.jsx
+++ src/components/EditProfile/Navbar.jsx
@@ -28,7 +28,7 @@ import Test from "../Sidebar"
 export default function WithSubnavigation() {
   //const { isOpen, onToggle } = useDisclosure()
   return (
-    <Box width={{ base: '100vw', md: '100vw' }}>
+    <Box width={{ base: '99vw', md: '99vw' }}>
       <Flex
         bg={useColorModeValue("white", "gray.800")}
         color={useColorModeValue("gray.600", "white")}
