4ea8668d539dc286023e74ef6b77361dd1f7201d
Abdul Mateen <mateen.2k19@gmail.com>
2023-10-13 22:33:08 +0500

    Pyschological Profile Updated

--- src/components/ClientFolder/MoodTable.jsx
+++ src/components/ClientFolder/MoodTable.jsx
@@ -1,4 +1,4 @@
-import { Table, Thead, Tbody, Tr, Th, Td } from "@chakra-ui/react";
+import { Text,Table, Thead, Tbody, Tr, Th, Td } from "@chakra-ui/react";
 import colors from "../Colors";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
@@ -31,28 +31,34 @@ const EmotionsTable = () => {
   }, [id]);
 
   return (
-    <Table variant="striped" boxShadow={'lg'}>
-      <Thead>
-        <Tr>
-          <Th color={colors.secondary}>Weekday</Th>
-          <Th color={colors.secondary}>Mood</Th>
-          <Th color={colors.secondary}>Emotion</Th>
-          <Th color={colors.secondary}>Specific Emotion</Th>
-          <Th color={colors.secondary}>Stress Level</Th>
-        </Tr>
-      </Thead>
-      <Tbody>
-        {userResponse.map((row, index) => (
-          <Tr key={index}>
-            <Td>{findDate(row.checkinDate)}</Td>
-            <Td>{row.emotion}</Td>
-            <Td>{row.reasonOfEmotion}</Td>
-            <Td>{row.specificEmotion}</Td>
-            <Td>{row.stressTimeline}</Td>
+    <>
+      <Text fontWeight={"700"} fontSize={25} marginLeft={"4%"}>
+        Daily Logs
+      </Text>
+      <Text marginLeft={"4%"} fontWeight={'500'}>Mood Check In are displayed here</Text>
+      <Table variant="striped" boxShadow={'lg'}>
+        <Thead>
+          <Tr>
+            <Th color={colors.secondary}>Weekday</Th>
+            <Th color={colors.secondary}>Mood</Th>
+            <Th color={colors.secondary}>Emotion</Th>
+            <Th color={colors.secondary}>Specific Emotion</Th>
+            <Th color={colors.secondary}>Stress Level</Th>
           </Tr>
-        ))}
-      </Tbody>
-    </Table>
+        </Thead>
+        <Tbody>
+          {userResponse.map((row, index) => (
+            <Tr key={index}>
+              <Td>{findDate(row.checkinDate)}</Td>
+              <Td>{row.emotion}</Td>
+              <Td>{row.reasonOfEmotion}</Td>
+              <Td>{row.specificEmotion}</Td>
+              <Td>{row.stressTimeline}</Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </>
   );
 };
 
