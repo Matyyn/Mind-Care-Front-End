dcc884966763e98cadba39c5abba6fa8f60c2100
Abdul Mateen <mateen.2k19@gmail.com>
2023-10-13 22:47:54 +0500

    Redux Persistant Applied

--- src/components/redux/store.jsx
+++ src/components/redux/store.jsx
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; 
+import storage from 'redux-persist/lib/storage';
+
 import myReducer from './slices/accountsReducer';
 import commentsReducer from './slices/commentsReducer';
 import postsReducer from './slices/postsReducer';
@@ -22,7 +23,7 @@ const rootReducer = combineReducers({
 
 const persistConfig = {
   key: 'root',
-  storage,  
+  storage,
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
