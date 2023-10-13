3f1f68a0badb4b1ebeaac3934f555b90c3f4dcda
Abdul Mateen <mateen.2k19@gmail.com>
2023-10-13 22:15:54 +0500

    Redux Persistant Used

--- src/components/redux/store.jsx
+++ src/components/redux/store.jsx
@@ -1,20 +1,34 @@
-import { configureStore } from '@reduxjs/toolkit'
-import myReducer from './slices/accountsReducer'
-import commentsReducer from './slices/commentsReducer'
-import postsReducer from './slices/postsReducer'
-import therapistReducer from './slices/therapistReducer'
-import appealsReducer from './slices/appealsReducer'
-import clientsAccountReducer from './slices/clientsAccountsReducer'
-import selectedAccounts from './slices/selectedAccounts'
+import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; 
+import myReducer from './slices/accountsReducer';
+import commentsReducer from './slices/commentsReducer';
+import postsReducer from './slices/postsReducer';
+import therapistReducer from './slices/therapistReducer';
+import appealsReducer from './slices/appealsReducer';
+import clientsAccountReducer from './slices/clientsAccountsReducer';
+import selectedAccounts from './slices/selectedAccounts';
+
+const rootReducer = combineReducers({
+  myReducer: myReducer,
+  commentsReducer: commentsReducer,
+  postsReducer: postsReducer,
+  appealsReducer: appealsReducer,
+  therapistReducer: therapistReducer,
+  clientsAccountReducer: clientsAccountReducer,
+  selectedAccounts: selectedAccounts,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,  
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    myReducer:myReducer,
-    commentsReducer:commentsReducer,
-    postsReducer:postsReducer,
-    appealsReducer:appealsReducer,
-    therapistReducer:therapistReducer,
-    clientsAccountReducer:clientsAccountReducer,
-    selectedAccounts:selectedAccounts
-  },
-})
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
